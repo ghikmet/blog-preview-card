f2c6b264927d462ccbdc301c51a7557e5298d334
Hikmet <guliyevhikmet@gmail.com>
2025-08-07 17:08:30 +0400

    update commit

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-const toggleButton = document.getElementById('header__navbar_toggle');
+  const toggleButton = document.getElementById('header__navbar_toggle');
   const navMenu = document.getElementById('header__navbar');
 
   toggleButton.addEventListener('click', () => {
-      navMenu.classList.toggle('hidden');
+    const isExpanded = toggleButton.getAttribute('aria-expanded') === 'true';
+    toggleButton.setAttribute('aria-expanded', String(!isExpanded)); // Update aria-expanded
+    navMenu.classList.toggle('hidden'); // Show/hide menu
   });
