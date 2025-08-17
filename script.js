ea68ba044cf0d79ea1a61553d4c8784b57361d63
Hikmet <guliyevhikmet@gmail.com>
2025-08-17 13:38:47 +0400

    Update commit

--- script.js
+++ script.js
@@ -1,8 +1,43 @@
-  const toggleButton = document.getElementById('header__navbar_toggle');
-  const navMenu = document.getElementById('header__navbar');
+const toggleButton = document.getElementById('header__navbar_toggle');
+const navMenu = document.getElementById('header__navbar');
+const navLinks = document.querySelectorAll('.header__link');
+const firstLink = document.getElementById('home');
 
-  toggleButton.addEventListener('click', () => {
+toggleButton.addEventListener('click', () => {
     const isExpanded = toggleButton.getAttribute('aria-expanded') === 'true';
-    toggleButton.setAttribute('aria-expanded', String(!isExpanded)); // Update aria-expanded
-    navMenu.classList.toggle('hidden'); // Show/hide menu
-  });
+    toggleButton.setAttribute('aria-expanded', String(!isExpanded));
+    navMenu.classList.toggle('hidden');
+
+    if (navMenu.classList.contains('hidden')) {
+        toggleButton.setAttribute('aria-expanded', 'false');
+    }else {
+        toggleButton.setAttribute('aria-expanded', 'true');
+    }
+});
+
+navLinks.forEach((link) => {
+    if (link.href === window.location.href) {
+        link.setAttribute('aria-current', 'page');
+    }
+});
+
+const baseURI = document.baseURI;
+const homePageLink = document.getElementById('home');
+
+window.addEventListener('load', () => {
+    if(baseURI) {
+        homePageLink.setAttribute('aria-current', 'page');
+    }
+});
+
+function showMenu() {
+    if (aria-expanded === 'false') {
+        navMenu.classList.remove('hidden');
+        firstLink.focus();
+    }else {
+        navMenu.classList.add('hidden');
+    }
+    
+}
+
+document.getElementById('header__navbar_toggle').addEventListener('click', showMenu);
