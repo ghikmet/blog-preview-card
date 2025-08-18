6b431bd297367621086d74f17de4b43c07e9f04b
Hikmet <guliyevhikmet@gmail.com>
2025-08-18 07:37:50 +0400

    Update commit

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const toggleButton = document.getElementById('header__navbar_toggle');
-const navbar = document.getElementById('header__navbar');
+const navMenu = document.getElementById('header__navbar');
+// const navbar = document.getElementById('header__navbar');
 
 
 toggleButton.addEventListener('click', () => {
-  const isExpanded = toggleButton.getAttribute('aria-expanded') === 'true';
-  toggleButton.setAttribute('aria-expanded', String(!isExpanded));
-  navbar.classList.toggle('hidden');
-  navbar.setAttribute('aria-hidden', String(isExpanded)); // true when hidden, false when visible
-});
+    const isExpanded = toggleButton.getAttribute('aria-expanded') === 'true';
+    toggleButton.setAttribute('aria-expanded', String(!isExpanded)); // Update ARIA state
+    navMenu.classList.toggle('hidden') = isExpanded; // Show/hide menu
+  });
 
 const navLinks = document.querySelectorAll('.header__link');
 
@@ -28,7 +28,7 @@ window.addEventListener('load', () => {
 });
 
 const firstLink = document.getElementById('home');
-const navMenu = document.getElementById('header__navbar');
+
 
 function showMenu() {
     if (aria-expanded === 'false') {
