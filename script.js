99348fbe5bfdb2419cb630d9a26879d3a4bd3d68
Hikmet <guliyevhikmet@gmail.com>
2025-09-05 20:11:14 +0400

    Update commit

--- script.js
+++ script.js
@@ -1,37 +1,51 @@
 const toggleButton = document.getElementById('header__navbar_toggle');
 const navMenu = document.getElementById('header__navbar');
-const navbar = document.getElementById('header__navbar');
-
+// const navbar = document.getElementById('header__navbar');
 
 toggleButton.addEventListener('click', () => {
     const isExpanded = toggleButton.getAttribute('aria-expanded') === 'true';
-    toggleButton.setAttribute('aria-expanded', String(!isExpanded));
-    navMenu.classList.toggle('hidden');
-    navMenu.setAttribute('aria-hidden', String(isExpanded));
-  });
-
-const navLinks = document.querySelectorAll('.header__link');
-
-
-navLinks.forEach((link) => {
-    if (link.href === window.location.href) {
-        link.setAttribute('aria-current', 'page');
+    if (isExpanded) {
+        navMenu.classList.add('hidden');
+        toggleButton.setAttribute('aria-expanded', 'false');
+        navMenu.setAttribute('aria-hidden', 'true');
+    } else {
+        navMenu.classList.remove('hidden');
+        toggleButton.setAttribute('aria-expanded', 'true');
+        navMenu.setAttribute('aria-hidden', 'false');
     }
 });
 
-const baseURI = document.baseURI;
-const homePageLink = document.getElementById('home');
 
-window.addEventListener('load', () => {
-    if(baseURI) {
-        homePageLink.setAttribute('aria-current', 'page');
-    }
-});
+// toggleButton.addEventListener('click', () => {
+//     const isExpanded = toggleButton.getAttribute('aria-expanded') === 'true';
+//     toggleButton.setAttribute('aria-expanded', String(!isExpanded));
+//     navMenu.classList.toggle('hidden');
+//     navMenu.setAttribute('aria-hidden', String(isExpanded));
+//   });
+
+// const navLinks = document.querySelectorAll('.header__link');
+
+
+// navLinks.forEach((link) => {
+//     if (link.href === window.location.href) {
+//         link.setAttribute('aria-current', 'page');
+//     }
+// });
+
+// const baseURI = document.baseURI;
+// const homePageLink = document.getElementById('home');
+
+// window.addEventListener('load', () => {
+//     if(baseURI) {
+//         homePageLink.setAttribute('aria-current', 'page');
+//     }
+// });
 
 document.addEventListener('keydown', (event) => {
     if (event.key === 'Escape') {
         navMenu.classList.add('hidden');
         toggleButton.setAttribute('aria-expanded', 'false');
+        navMenu.setAttribute('aria-hidden', 'true');
     }
 });
 
